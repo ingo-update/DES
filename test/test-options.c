028b8e9dfad4e2f4b9facab4a648994ea670f916
ingo <ingo@update.uu.se>
2021-05-16 19:52:04 +0200

    Reorganize test-options

--- test/test-options.c
+++ test/test-options.c
@@ -7,18 +7,19 @@
 /* Some option arrays */
 char *short_key[] = {"test", "abcd"};
 char *long_key[] = {"test", "12345678901234567890"};
-char *illegal_option[] = {"test", "-a", "-b"};
+
 char *bad_key[] = {"test", "abcdefghijklmnop"};
 char *good_key[] = {"test", "1111222233334444"};
+
+char *illegal_option[] = {"test", "-a", "-b"};
 char *decrypt[] = {"test", "1111222233334444", "-d"};
+
 char *infile[] = {"test", "-i", "foo"};
 char *outfile[] = {"test", "-o", "bar"};
 
-/* TODO Split into several functions for clarity */
 int test_options()
 {
   int fail;
-  unsigned int a, b;
   struct options o1;
 
   fail = 0;
@@ -27,62 +28,6 @@ int test_options()
     {
       ++fail;
       fprintf(stderr, "FAIL: Accepted empty options.\n");
-    }
-
-  o1 = parse_options(2, short_key);
-  if (0 != o1.valid)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Accepted too short key '%s'.\n", short_key[0]);
-    }
-
-  o1 = parse_options(2, long_key);
-  if (0 != o1.valid)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Accepted too long key '%s'.\n", long_key[0]);
-    }
-
-  o1 = parse_options(3, illegal_option);
-  if (0 != o1.valid)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Accepted illegal option '%s'.\n", illegal_option[0]);
-    }
-
-  o1 = parse_options(2, bad_key);
-  a = (o1.key >> 32) & 0xffffffff;
-  b = o1.key & 0xffffffff;
-  if (0 != o1.valid)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Accepted bad key '%s' as 0x%x%x.\n", bad_key[1], a, b);
-    }
-
-  o1 = parse_options(2, good_key);
-  a = (o1.key >> 32) & 0xffffffff;
-  b = o1.key & 0xffffffff;
-  if (0 == o1.valid)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Did not accept good key '%s'.\n", good_key[1]);
-    }
-  else if (a != 0x11112222 || b != 0x33334444)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Read good key '%s' as 0x%x%x.\n", good_key[1], a, b);
-    }
-  if (1 == o1.decrypt)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Set decrypt mode without decrypt flag -d present.\n");
-    }
-
-  o1 = parse_options(3, decrypt);
-  if (0 == o1.decrypt)
-    {
-      ++fail;
-      fprintf(stderr, "FAIL: Did not recognize decrypt flag -d.\n");
     }
 
   o1 = parse_options(3, infile);
@@ -102,6 +47,73 @@ int test_options()
   return fail;
 }
 
+int test_key()
+{
+  int fail;
+  int a,b;
+  struct options o1;
+
+  fail = 0;
+  o1 = parse_options(2, short_key);
+  if (0 != o1.valid)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Accepted too short key '%s'.\n", short_key[0]);
+    }
+
+  o1 = parse_options(2, long_key);
+  if (0 != o1.valid)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Accepted too long key '%s'.\n", long_key[0]);
+    }
+
+  o1 = parse_options(2, good_key);
+  a = (o1.key >> 32) & 0xffffffff;
+  b = o1.key & 0xffffffff;
+  if (0 == o1.valid)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Did not accept good key '%s'.\n", good_key[1]);
+    }
+  else if (a != 0x11112222 || b != 0x33334444)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Read good key '%s' as 0x%x%x.\n", good_key[1], a, b);
+    }
+
+  return fail;
+}
+
+int test_flags()
+{
+  int fail;
+  struct options o1;
+
+  fail = 0;
+  o1 = parse_options(3, illegal_option);
+  if (0 != o1.valid)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Accepted illegal option '%s'.\n", illegal_option[0]);
+    }
+
+  if (1 == o1.decrypt)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Set decrypt mode without decrypt flag -d present.\n");
+    }
+
+  o1 = parse_options(3, decrypt);
+  if (0 == o1.decrypt)
+    {
+      ++fail;
+      fprintf(stderr, "FAIL: Did not recognize decrypt flag -d.\n");
+    }
+
+  return fail;
+}
+
 int main()
 {
   int fail;
@@ -110,6 +122,8 @@ int main()
   fail = 0;
 
   fail += test_options();
+  fail += test_key();
+  fail += test_flags();
 
   if (0 == fail)
     {
