bca83a0552f86cc7c31a50f611870e499fa08e6b
Ingemar Åberg <ingo@update.uu.se>
2021-08-11 15:18:55 +0200

    Remove file path from printed source data files

--- src/tools/datafile.rb
+++ src/tools/datafile.rb
@@ -1,7 +1,7 @@
 # Assume all arguments are filenames, output the name and the content.
 
 ARGV.each do |name|
-  puts "File #{name}:\n"
+  puts "File #{File.basename(name)}:\n"
   File.open(name) do |file|
     file.each_line{ |line| puts line }
   end
